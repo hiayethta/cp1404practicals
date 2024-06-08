279eb98d4a81613b935eae0df9dede1c367bfe6f
margareth tajonera <135513507+hiayethta@users.noreply.github.com>
2024-06-08 10:40:06 +1000

    Add files via upload
    Practical 3 deliverables

---
+++ prac_03/exceptions_to_complete.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 - Practical
+Fill in the TODOs to complete the task
+"""
+
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+        is_finished = True
+    except ValueError:
+        print("Please enter a valid integer.")
+
+print("Valid result is:", result)
+
+# try:
+#     numerator = int(input("Enter the numerator: "))
+#     denominator = int(input("Enter the denominator: "))
+#     fraction = numerator / denominator
+#     print(fraction)
+# except ValueError:
+#     print("Numerator and denominator must be valid numbers!")
+# except ZeroDivisionError:
+#     denominator = 2  # set denominator to default value
+# print("Finished.")
---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+"""
+CP1404 - Practical
+Practice using random module and python console
+"""
+
+import random
+
+print(random.randint(5, 20))  # line 1
+# Smallest number: 5, Largest number: 20
+
+print(random.randrange(3, 10, 2))  # line 2
+#  Smallest number: 3. Largest number: 9
+# Could not have produced '4'. Only odd numbers can be printed on this line
+
+print(random.uniform(2.5, 5.5))  # line 3
+# Smallest number: 2.5, Largest number: 5.5
