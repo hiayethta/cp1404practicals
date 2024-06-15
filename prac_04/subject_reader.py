264e0c0bc50bf926f32e1ad90127f0fa1bdd1383
hiayethta <tajonera1643@gmail.com>
2024-06-15 10:36:50 +1000

    Add function to display subjects and modify load data function to return data as nested list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,11 +8,12 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    display_subjects(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []  # Initialise empty list
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +23,16 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subject.append(parts)
         print("----------")
     input_file.close()
+    return subject
+
+
+def display_subjects(subjects):
+    """Display subjects formatted into a sentence"""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students.")
 
 
 main()
