1b08ede274602f97bae23613fd3ed3a71f3c727a
hiayethta <tajonera1643@gmail.com>
2024-06-15 10:49:15 +1000

    Add security checker program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,13 +4,31 @@ Program that prompts user for 5 numbers and stores in a list
 Then prints first, last, min, max, and average of numbers
 """
 
-numbers = []
-for i in range(5):
-    number = int(input("Number: "))
-    numbers.append(number)
+# numbers = []
+# for i in range(5):
+#     number = int(input("Number: "))
+#     numbers.append(number)
+#
+# print(f"The first number is {numbers[0]}")
+# print(f"The last number is {numbers[-1]}")
+# print(f"The smallest number is {min(numbers)}")
+# print(f"The largest number is {max(numbers)}")
+# print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
-print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is {min(numbers)}")
-print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+"""
+CP1404 Practical
+Security checker program
+"""
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+while username != "":
+    if username in usernames:
+        print("Access granted")
+        username = input("Username: ")  # user can enter another username
+    elif username not in usernames:
+        print("Access denied")
+        username = input("Username: ")
+print("Program finished.")  # end program if input is blank
