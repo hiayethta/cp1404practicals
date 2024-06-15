258fd96fdaf24b6e0887e4d49ea0d1a522251ded
hiayethta <tajonera1643@gmail.com>
2024-06-15 10:43:02 +1000

    Use shortcut to format file

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,5 +13,4 @@ print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
-
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
