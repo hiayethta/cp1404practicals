e94eb6eca54d4658a7e3c2dbb59b277483c1daf7
hiayethta <tajonera1643@gmail.com>
2024-06-22 13:21:37 +1000

    Add for loop to print formatted state codes and names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
@@ -16,3 +15,10 @@ while state_code != "":
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
+
+# Loop that prints all state names neatly lined up
+states_list = list(CODE_TO_NAME.items())
+print(states_list)
+for state_code in states_list:
+    print(f"{state_code[0]:4} is {state_code[1]}")
+
