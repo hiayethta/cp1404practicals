75240ab1e33c1aeb4b8e95772d94c8b0a7e0aeea
hiayethta <tajonera1643@gmail.com>
2024-06-22 13:50:53 +1000

    Complete prac 5 hex colours exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -6,3 +6,11 @@ Use constant dictionary to return hex colour code
 COLOUR_TO_HEX_CODE = {"Red": "#ff0000", "Green": "#00ff00", "Blue": "#0000ff", "Dark Brown": "#654321",
                       "Gray": "bebebe", "Aqua": "00fff", "Lilac": "#c8a2c8", "Mint": "#3eb489", "Mustard": "#ffdb58",
                       "Ruby": "#e0115f"}
+
+colour = input("Enter colour: ").title()
+while colour != "":
+    try:
+        print(colour, "is", COLOUR_TO_HEX_CODE[colour])
+    except KeyError:
+        print("Invalid colour entered.")
+    colour = input("Enter colour: ")
