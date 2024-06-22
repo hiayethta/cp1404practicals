e83a9c27948b3010c2cbe1356d079f4be95d7e4a
hiayethta <tajonera1643@gmail.com>
2024-06-22 13:46:52 +1000

    Add colour to hex code dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,8 @@
+"""
+CP1404/CP5632 Practical
+Use constant dictionary to return hex colour code
+"""
+
+COLOUR_TO_HEX_CODE = {"Red": "#ff0000", "Green": "#00ff00", "Blue": "#0000ff", "Dark Brown": "#654321",
+                      "Gray": "bebebe", "Aqua": "00fff", "Lilac": "#c8a2c8", "Mint": "#3eb489", "Mustard": "#ffdb58",
+                      "Ruby": "#e0115f"}
