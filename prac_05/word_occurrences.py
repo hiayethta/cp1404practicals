dee31be04d219b5b2d55449f351b2945005c9895
hiayethta <tajonera1643@gmail.com>
2024-06-23 14:03:18 +1000

    Delete functions from code, complete program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,34 +7,6 @@ Actual:   32 minutes
 
 PUNCTUATION_MARKS = ".,;:'!?-"
 
-# def main():
-#     string = input("Text: ")
-#     word_counts = count_words(string)
-#     count_words(word_counts)
-#
-#
-# def format_output(word_counts):
-#     max_word_length = max(len(word) for word in word_counts.keys())
-#     for word in sorted(word_counts.key()):
-#         print(f"{word:{max_word_length}} : {word_counts[word]}")
-#
-#
-# def count_words(string):
-#     string = string.lower()
-#     words = string.split()
-#     word_count = {}
-#     for word in words:
-#         word = word.strip(PUNCTUATION_MARKS)
-#         if word in word_count:
-#             word_count[word] += 1
-#         else:
-#             word_count[word] = 1
-#
-#     return word_count
-#
-#
-# main()
-
 string = input("Text: ")
 string = string.lower()
 words = string.split()
