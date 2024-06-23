91070787ed27c0d0aae3d4660b342581a86ab5f6
hiayethta <tajonera1643@gmail.com>
2024-06-23 14:30:59 +1000

    Add file-processing code

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,3 +4,11 @@ File reading, data processing and formatting output
 Data based on Wimbledon gentleman's singles champions
 """
 
+FILENAME = "wimbledon.csv"
+
+records = []
+with open(FILENAME, "r", encoding="utf-8") as in_file:
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split(",")
+        records.append(parts)
