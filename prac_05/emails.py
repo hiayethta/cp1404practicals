43cb101d129b3f32a8703953aabec7cf8c33bdde
hiayethta <tajonera1643@gmail.com>
2024-06-23 13:36:21 +1000

    Extract method to create main() function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,10 +4,44 @@ Store user emails and names in dictionary
 Extract name from email
 """
 
-EMAIL_TO_NAME = {}
-emails = []
-email = input("Email: ")
-while email != "":
-    name = email.split('@')[0]
+
+# EMAIL_TO_NAME = {}
+# emails = []
+# names = []
+# email = input("Email: ")
+# while email != "":
+#     name = email.split('@')[0]
+#     emails.append(email)
+#     name_uppercase = name.title()
+#     # if '.' in name_uppercase:
+#     #     name = email.split('.')
+#     #     name_confirmation = input(f"Is your name {name[0]} {name[1]}? (Y/n) ").upper()
+#     name_confirmation = input(f"Is your name {name_uppercase}? (Y/n) ").upper()
+#     if name_confirmation == "Y":
+#         names.append(name_uppercase)
+#         print(names)
+#         email = input("Email: ")
+#     elif name_confirmation == "N":
+#         name = input("Name: ")
+#         print(name.title())
+#         email = input("Email: ")
+
+
+def main():
+    """Ask user for email and name and store into dictionary."""
+    email_to_name = {}
     email = input("Email: ")
-    print(name.title())
+    while email != "":
+        name = extract_name_from_email(email)
+        name_confirmation = input(f"Is your name {name}? (Y/n) ")
+        if name_confirmation == "Y":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+
+def extract_name_from_email(email):
+    """Extract name from email."""
+
+
+main()
