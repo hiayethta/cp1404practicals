a6ded590ca66685241ab0721320f3c6c8df033c0
hiayethta <tajonera1643@gmail.com>
2024-06-23 13:58:57 +1000

    Write functions to count words and print formatted output

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,37 @@
+"""
+Word Occurrences
+Program to count the occurrences of each word in a string.
+Estimate: 20 minutes
+Actual:   32 minutes
+"""
+
+PUNCTUATION_MARKS = ".,;:'!?-"
+
+
+def main():
+    string = input("Text: ")
+    word_counts = count_words(string)
+    count_words(word_counts)
+
+
+def format_output(word_counts):
+    max_word_length = max(len(word) for word in word_counts.keys())
+    for word in sorted(word_counts.key()):
+        print(f"{word:{max_word_length}} : {word_counts[word]}")
+
+
+def count_words(string):
+    string = string.lower()
+    words = string.split()
+    word_count = {}
+    for word in words:
+        word = word.strip(PUNCTUATION_MARKS)
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    return word_count
+
+
+main()
