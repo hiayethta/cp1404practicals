06921802c95411aa563d1983cda135bf30a2759a
hiayethta <tajonera1643@gmail.com>
2024-07-14 15:31:22 +1000

    Initialise guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,8 @@
+"""CP1404 Practical - Guitar class"""
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0.0):
+        self.name = name
+        self.year = year
+        self.cost = cost
