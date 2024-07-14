36c0e72a12cd6123e0bf7c7e5744bd9ee5c13312
hiayethta <tajonera1643@gmail.com>
2024-07-14 16:39:28 +1000

    Add loop that prints guitar details

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -3,7 +3,7 @@ CP1404 Practical -
 Program that stores user's guitars and prints their details
 
 Estimated Completion: 40 minutes
-Actual:
+Actual: 28~ minutes
 """
 from prac_06.guitar import Guitar
 
@@ -20,8 +20,16 @@ def main():
         guitars.append(new_guitar_entry)
 
         name = input("Name: ")
-    else:
-        None
+
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("These are my guitars: ")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = ""
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
 main()
