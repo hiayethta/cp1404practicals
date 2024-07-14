9cdf0330ba9d9fc6e8057da017e1c9be95f9212a
hiayethta <tajonera1643@gmail.com>
2024-07-14 15:20:12 +1000

    Add __str__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -9,9 +9,11 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        return f"{self.field}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
     def is_dynamic(self):
         if self.typing == "Dynamic":
             return True
         else:
             return False
-
