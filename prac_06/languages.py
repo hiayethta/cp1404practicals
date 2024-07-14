f709688cd879d685cb396fd74911ba8cc154f771
hiayethta <tajonera1643@gmail.com>
2024-07-14 15:18:19 +1000

    Add data to program

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,3 +5,10 @@ the dynamic programming languages.
 Estimated Completion: 35 minutes (3:08pm)
 Actual:
 """
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
