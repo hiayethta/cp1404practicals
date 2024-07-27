2d1a0d029d2958c2cd2f40b3113a846798bde88e
hiayethta <tajonera1643@gmail.com>
2024-07-28 08:57:17 +1000

    add text entry field to button handler function

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,8 +9,7 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        print("test")
-        self.root.ids.output_label.text = "Hello"
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 
 BoxLayoutDemo().run()
