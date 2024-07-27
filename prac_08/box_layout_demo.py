b4950bb49e019d36a0d5ace3b42879869c25bcc9
hiayethta <tajonera1643@gmail.com>
2024-07-28 08:55:34 +1000

    feat function to handle greet

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        print("test")
+        self.root.ids.output_label.text = "Hello"
+
+
+BoxLayoutDemo().run()
