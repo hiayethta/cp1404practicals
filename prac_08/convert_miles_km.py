f9054ac3b058bc96b560adef9814f1e2e5b9ebf5
hiayethta <tajonera1643@gmail.com>
2024-07-28 22:56:18 +1000

    Add function to get valid miles

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -14,6 +14,7 @@ from kivy.lang import Builder
 
 class MilesToKilometres(App):
     """ MilesToKilometres is a Kivy app for converting miles to kilometres """
+
     def build(self):
         """ Build Kivy app from kv file"""
         self.title = "Convert miles to kilometres"
@@ -24,6 +25,17 @@ class MilesToKilometres(App):
         """Handle calculation from miles to kilometres, output result to label"""
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
+
+    def get_valid_miles(self):
+        """
+        get text input from widget and convert to float
+        :return: 0 if error, float of text if valid
+        """
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
 
 
 # create and start the App running
