43773ab7a450fa8c4f51d0824c31b07cc9cf957e
hiayethta <tajonera1643@gmail.com>
2024-07-28 22:47:18 +1000

    Edit func for building app

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -11,10 +11,17 @@ from kivy.lang import Builder
 
 
 class MilesToKilometres(App):
+    """ MilesToKilometres is a Kivy app for converting miles to kilometres """
+
     def build(self):
-        self.title = "Layout Hints Demo"
+        """ Build Kivy app from kv file"""
+        self.title = "Convert miles to kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_update(self):
+        """Handle changes to the text input by updating the model from the view."""
+        self.message = self.root.ids.user_input.text
 
 
 # create and start the App running
