8c0ba0c741093deb7bfccf57254cebaa26a0394e
hiayethta <tajonera1643@gmail.com>
2024-07-28 22:53:20 +1000

    Add function to calculate M to KM

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,22 +6,24 @@ Estimated time to complete: 40 minutes
 Actual:
 """
 
+MILES_TO_KM = 1.60934
+
 from kivy.app import App
 from kivy.lang import Builder
 
 
 class MilesToKilometres(App):
     """ MilesToKilometres is a Kivy app for converting miles to kilometres """
-
     def build(self):
         """ Build Kivy app from kv file"""
         self.title = "Convert miles to kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_update(self):
-        """Handle changes to the text input by updating the model from the view."""
-        self.message = self.root.ids.user_input.text
+    def handle_calculate(self):
+        """Handle calculation from miles to kilometres, output result to label"""
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
 
 
 # create and start the App running
