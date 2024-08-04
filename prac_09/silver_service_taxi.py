ebc340fe3dae6f1315dad2192e468d7286d7c465
hiayethta <tajonera1643@gmail.com>
2024-08-04 23:51:49 +1000

    Upload files

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -16,6 +16,10 @@ class SilverServiceTaxi(Taxi):
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
+    def __str__(self):
+        """Return a string representation of a SilverServiceTaxi."""
+        return f"{super.__str__()} plus flagfall of ${self.flagfall:.2f}"
+
     def get_fare(self):
         """Get the current fare with extra charge"""
         return self.flagfall + super().get_fare()
