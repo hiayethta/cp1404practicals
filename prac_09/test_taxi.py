05337aa4e311fc197303c6be4fb69db62d8b10da
hiayethta <tajonera1643@gmail.com>
2024-08-04 21:56:26 +1000

    Add Taxi object for testing

---
+++ prac_09/test_taxi.py
@@ -0,0 +1,13 @@
+"""
+CP1404 Practical
+Testing file for Taxi
+"""
+
+from prac_09.taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi.drive(40)
+print(f"{my_taxi} {my_taxi.get_fare()}")  # Taxi details and current fare
+my_taxi.start_fare()  # Restart fare
+my_taxi.drive(100)
+print(f"{my_taxi} {my_taxi.get_fare()}")
