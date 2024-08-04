578f8ba5a21c7dc01366e18677e75651ac951b83
hiayethta <tajonera1643@gmail.com>
2024-08-04 22:05:19 +1000

    Use class variable instead of passing in value

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -5,7 +5,7 @@ Testing file for Taxi
 
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi("Prius 1", 100)
 my_taxi.drive(40)
 print(f"{my_taxi} {my_taxi.get_fare()}")  # Taxi details and current fare
 my_taxi.start_fare()  # Restart fare
